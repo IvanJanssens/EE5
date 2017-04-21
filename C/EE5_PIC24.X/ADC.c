b88bc57c62b0224cf0d739da345bb54c30bc796e
Jerom Van Derbeken <jerom.vanderbeken@gmail.com>
2017-04-21 14:12:22 +0200

    ADC tested, but more testing required

--- C/EE5_PIC24.X/ADC.c
+++ C/EE5_PIC24.X/ADC.c
@@ -2,18 +2,58 @@
 #include <stdlib.h>
 #include <xc.h>
 
+#define VO_B RB5
+#define ANSVO_B ANSB5
+#define TRISVO_B TRISB5
+#define VO_A RB14
+#define ANSVO_A ANSB14
+#define TRISVO_A TRISB14
+#define VO_MM RD2
+#define ANSVO_MM ANSD2
+#define TRISVO_MM TRISD2
+
 
 void init_ADC(void) {
+    ANSB = 0;
     TRISB = 0;
-    TRISBbits.TRISB0 = 1;
+    TRISD = 0;
+    ANSBbits.ANSVO_B = 1;
+    ANSBbits.ANSVO_A = 1;
+    ANSDbits.ANSVO_MM = 1;
+    TRISBbits.TRISVO_B = 1;
+    TRISBbits.TRISVO_A = 1;
+    TRISDbits.TRISVO_MM = 1;
+    
     ADCON1bits.ADON = 0;
-    ADCON1 = 0x1001; // 0001 0000 0000 0001
-    ADCON2 = 0x0300; // 0000 0011 0000 0000
-    ADCON3 = 0x0001; // 0000 0000 0001 0000
+    ADCON1 = 0x1041; // 0001 0000 0100 0001 checked
+    ADCON2 = 0x0300; // 0000 0011 0000 0000 checked
+    ADCON3 = 0x0001; // 0000 0000 0001 0000 checked
+    int i;
+    for(i = 20; i>0; i--) {
+        ADCON1bits.ADCAL = 1;
+    }
     ADCON1bits.ADON = 1;
     while(!ADSTATHbits.ADREADY);
+    
+    ADL0CONH = 0xA001; // 1010 0000 0000 0001
+    ADL0CONL = 0x2202; // 0010 0010 0000 0010 : 3 sample registers
+    ADTBL0 = 0x0019; // 0000 0000 0001 1001 ==>RD2: Multimeter
+    ADTBL1 = 0x000E; // 0000 0000 0000 1110 ==>RB14: VOA
+    ADTBL2 = 0x0005; // 0000 0000 0000 0101 ==>RB5: VOB
+    ACCONH = 0;
+    
+   //for interrupts
+    INTCON1bits.NSTDIS = 1; //no nesting interrupts
+    IFS0= 0; //setting the flag on zero
+    PORTD = 0;
+    IPC3bits.AD1IP = 7; //Highest priority
+    IEC0bits.AD1IE = 1; //enabling interrupts
+
+    
+    ADL0STAT = 0; //clearing the ADLIF: A/D Sample List Interrupt Event Flag bit
+    ADSTATL = 0; //clearing all interrupt flag bits
 }
 void ADC() {
-    
+    ADL0CONLbits.SLEN = 1;
 }
 
--- C/EE5_PIC24.X/main.c
+++ C/EE5_PIC24.X/main.c
@@ -1,27 +1,24 @@
-/* 
- * File:   main.c
- * Author: Jerom Van Derbeken
- *
- * Created on March 3, 2017, 2:57 PM
- */
-
 #include <xc.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "config_EE5.h"
 #include "ADC.h"
 
+#define CLOCK_FREQ 20000000ULL
 
 void initChip(void);
 void counter(void);
+void _ISR _ADC1Interrupt(void);
+int count = 0;
 
 int main(void) {
     initChip();
-    //initADC();
-    //ADC();
+    init_ADC();
+    
     while(1){
-        counter();
-        PORTB = 0xFFFF;
+        ADC();
+        //counter();
+        //PORTB = 0xFFFF;
         //PORTBbits.RB15 = 0;
         //PORTBbits.RB13 = 1;
     }
@@ -42,11 +39,33 @@ void initChip(void) {
     
 }
 
+void _ISR _ADC1Interrupt(void){
+    
+    //__delay_ms(200);
+    PORTBbits.RB0 = 0;
+    if (IFS0bits.AD1IF == 1) {
+        IFS0bits.AD1IF = 0;
+        TRISBbits.TRISB14 = 1;
+        count++;
+        PORTB = ADRES0*16; // & 0x0E00; //0000 1110 0000 0000
+        //PORTB = count;
+        TRISBbits.TRISB0 = 0;
+        PORTBbits.RB0 = 1;
+    } 
+    else {
+        TRISDbits.TRISD9 = 0;
+        PORTDbits.RD9 = 1;
+    }
+    
+    
+}
+
 void counter(void) {
     int i = 0;
     int j = 0;
     unsigned int number = 16;
     PORTB = 1;
+    
 
     while (1) {
         for (i = 0; i < 5000; i++) {
